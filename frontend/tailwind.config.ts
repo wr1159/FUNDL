c86b98847952ed13218b7fdb3814c6bec5a017d0
wr1159 <chuweiron18@gmail.com>
2025-04-03 23:26:21 +0300

    Add neobrutalism theme

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,18 +1,44 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                main: "var(--main)",
+                overlay: "var(--overlay)",
+                bg: "var(--bg)",
+                bw: "var(--bw)",
+                blank: "var(--blank)",
+                text: "var(--text)",
+                mtext: "var(--mtext)",
+                border: "var(--border)",
+                ring: "var(--ring)",
+                ringOffset: "var(--ring-offset)",
+
+                secondaryBlack: "#212121",
+            },
+            borderRadius: {
+                base: "6px",
+            },
+            boxShadow: {
+                shadow: "var(--shadow)",
+            },
+            translate: {
+                boxShadowX: "5px",
+                boxShadowY: "4px",
+                reverseBoxShadowX: "-5px",
+                reverseBoxShadowY: "-4px",
+            },
+            fontWeight: {
+                base: "500",
+                heading: "700",
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 } satisfies Config;
