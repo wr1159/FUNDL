eab3e9d5c2294584c1fb46c7ebff6b991b2006ad
wr1159 <chuweiron18@gmail.com>
2025-04-03 23:44:35 +0300

    Use SAAS Template from Neobrutalism

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,9 +1,11 @@
 import type { Config } from "tailwindcss";
 
 export default {
+    darkMode: ["class"],
     content: [
         "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
         "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/sections/**/*.{js,ts,jsx,tsx,mdx}",
         "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     ],
     theme: {
@@ -19,7 +21,6 @@ export default {
                 border: "var(--border)",
                 ring: "var(--ring)",
                 ringOffset: "var(--ring-offset)",
-
                 secondaryBlack: "#212121",
             },
             borderRadius: {
@@ -37,6 +38,28 @@ export default {
             fontWeight: {
                 base: "500",
                 heading: "700",
+            },
+            keyframes: {
+                "accordion-down": {
+                    from: {
+                        height: "0",
+                    },
+                    to: {
+                        height: "var(--radix-accordion-content-height)",
+                    },
+                },
+                "accordion-up": {
+                    from: {
+                        height: "var(--radix-accordion-content-height)",
+                    },
+                    to: {
+                        height: "0",
+                    },
+                },
+            },
+            animation: {
+                "accordion-down": "accordion-down 0.2s ease-out",
+                "accordion-up": "accordion-up 0.2s ease-out",
             },
         },
     },
